0cb57aab84e94f1c5f5355b976c5d445d0dd56a4
mondalsabarna29-gif <mondalsubarna29@gmail.com>
2025-12-01 18:53:11 +0530

    fefvefcfedcece

--- routes/formSubmissions.js
+++ routes/formSubmissions.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const formSubmissionController = require('../controllers/formSubmissionController');
-const { protect, authorize } = require('../middleware/auth');
+const { auth, authorize } = require('../middleware/auth');
 
 // Public route - submit form
 router.post('/submit', formSubmissionController.submitForm);
 
 // Protected routes - admin only
-router.get('/', protect, authorize('admin'), formSubmissionController.getAllSubmissions);
-router.get('/:id', protect, authorize('admin'), formSubmissionController.getSubmission);
-router.put('/:id', protect, authorize('admin'), formSubmissionController.updateSubmission);
-router.delete('/:id', protect, authorize('admin'), formSubmissionController.deleteSubmission);
+router.get('/', auth, authorize('admin'), formSubmissionController.getAllSubmissions);
+router.get('/:id', auth, authorize('admin'), formSubmissionController.getSubmission);
+router.put('/:id', auth, authorize('admin'), formSubmissionController.updateSubmission);
+router.delete('/:id', auth, authorize('admin'), formSubmissionController.deleteSubmission);
 
 module.exports = router;
